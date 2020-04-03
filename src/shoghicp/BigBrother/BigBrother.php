dd53d368cc45b33f286291e50c710d9ccc2ca0dc
GamingFR91 <52374013+GamingFR91@users.noreply.github.com>
2020-04-03 17:51:24 +0200

    Update BigBrother.php

--- src/shoghicp/BigBrother/BigBrother.php
+++ src/shoghicp/BigBrother/BigBrother.php
@@ -81,7 +81,7 @@ class BigBrother extends PluginBase implements Listener{
 		}
 
 		if($enable){
-			if(Info::CURRENT_PROTOCOL === 282){
+			if(!Info::CURRENT_PROTOCOL === null){
 				ConvertUtils::init();
 
 				$this->saveDefaultConfig();
